9a2e9926c10da1ee21058ff5e6915775819c417e
delrocco <delrocco@users.noreply.github.com>
2023-06-14 17:28:03 -0400

    2 more example programs

---
+++ CoinFlip/Program.cs
@@ -0,0 +1,31 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Random prng = new Random();
+        int flip;
+        int heads = 0;
+        int tails = 0;
+
+        while (true)
+        {
+            flip = prng.Next();
+
+            if (flip % 2 == 0)
+            {
+                heads++;
+                Console.Write("Heads!");
+            }
+            else
+            {
+                tails++;
+                Console.Write("Tails!");
+            }
+
+            Console.WriteLine(" (" + heads + ", " + tails + ")");
+
+            Thread.Sleep(300);
+        }
+    }
+}
---
+++ Highest/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+namespace Highest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbas = new int[10];
+            Random prng = new Random();
+
+            for (int i = 0; i < numbas.Length; i++)
+            {
+                numbas[i] = prng.Next() % 1000;
+                Console.WriteLine(numbas[i]);
+            }
+
+            int highest = numbas[0];
+
+            for (int i=1; i<numbas.Length; i++)
+            {
+                if (numbas[i] > highest)
+                    highest = numbas[i];
+            }
+
+            Console.WriteLine("Highest value is: " + highest);
+        }
+    }
+}
