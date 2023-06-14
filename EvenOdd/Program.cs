76bcf74a1b1b7eed713134ddc0d6b99c6c0164e9
delrocco <delrocco@users.noreply.github.com>
2023-06-14 15:11:59 -0400

    solution and first two projects that we did at WPL

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EvenOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+
+            string? answer = Console.ReadLine();
+
+            int number = Convert.ToInt32(answer);
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even!");
+            }
+            else
+            {
+                Console.WriteLine("Odd!");
+            }
+        }
+    }
+}
